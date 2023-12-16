c905a857b34235a12bdc6292a00439358c1b942e
Gregor Corbin <g.corbin@fz-juelich.de>
2023-12-16 19:24:32 +0100

    test: Tests for problem

---
+++ problem_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestCheckSolution_WrongInput(t *testing.T) {
+	p := Problem{
+		problem: "x + 9 = 11",
+		solution: "2",
+	}
+
+	givenSolution := "3"
+	expected := false
+	check := p.checkSolution(givenSolution)
+	if check != expected {
+		t.Fatalf(`p.checkSolution("%s") = %t, want %t`, givenSolution, check, expected )
+	}
+}
+
+func TestCheckSolution_CorrectInput(t *testing.T) {
+	p := Problem{
+		problem: "x + 9 = 11",
+		solution: "2",
+	}
+
+	givenSolution := "2"
+	expected := true
+	check := p.checkSolution(givenSolution)
+	if check != expected {
+		t.Fatalf(`p.checkSolution("%s") = %t, want %t`, givenSolution, check, expected )
+	}
+}
+
+func TestEqAdd(t *testing.T) {
+	p := additionProblem()
+
+	if !p.checkSolution(p.solution){
+		t.Fatalf(`Problem '%s': Solution '%s' was not accepted`, p.problem, p.solution)
+	}
+}
